89d45ec31b57b1dbdcb4be0cbcd5473d0e46a6a9
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-09 12:19:59 +0100

    Update db.php

--- _config/db.php
+++ _config/db.php
@@ -1,3 +1,4 @@
 <?php
 
-$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
