3e65349479764d3f64e04fd16c38aee0cd9610a8
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-09 09:41:27 +0100

    add home

---
+++ models/home_model.php
@@ -0,0 +1,13 @@
+<?php
+
+
+//$user = new User(1);
+//$user->username = 'Baz';
+//$user->email = 'Baz';
+//$var = $user->email;
+//$user->setPassword('123');
+//$user->edit();
+//dd($user);
+
+//$users = User::getAll();
+//dd($users);
