ea2bf9a1a6d35d333f1af4d9cb3e836b2e8331cb
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-10 08:57:59 +0100

    add tag class

---
+++ models/Tag.php
@@ -0,0 +1,47 @@
+<?php
+
+class Tag
+{
+    public $tag_id;
+    public $tag;
+
+
+    public function __construct()
+    {
+
+    }
+
+    public function getTags(){
+        global $db;
+        $tags = $db->query('SELECT * FROM tags');
+        return $tags->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create($tag){
+        global $db;
+        $stmt = $db->prepare('INSERT INTO tags (tag) VALUES (:tag)');
+        $stmt->bindValue(':tag', $tag, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+    public function edit($tag_id, $tag){
+        $this->getTags();
+        foreach ($tags as $tag) {
+                $tag = $tag['tag'];
+        }
+
+        $this->query("UPDATE tags 
+                            SET tag = :tag, 
+                            WHERE tag_id = :tag_id");
+        $this->bind(':tag', tag_id);
+        $this->bind(':tag_id', $tag_id);
+
+        $this->execute();
+    }
+
+    public function delete($tag_id){
+        $this->query("DELETE FROM tags WHERE tag_id = :tag_id");
+        $this->bind(':tag_id', $tag_id);
+        $this->execute();
+    }
+
+}
