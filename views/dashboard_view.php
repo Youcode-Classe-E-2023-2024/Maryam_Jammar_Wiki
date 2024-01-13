e9bf294fb6c51344b29d5541efd08cb7d7b48e87
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-13 13:40:53 +0100

    show categories on dasboard

--- views/dashboard_view.php
+++ views/dashboard_view.php
@@ -24,11 +24,8 @@
                         <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
                             <div class="mb-4 flex items-center justify-between">
                                 <div>
-                                    <h3 class="text-xl font-bold text-gray-900 mb-2">Latest Transactions</h3>
-                                    <span class="text-base font-normal text-gray-500">This is a list of latest transactions</span>
-                                </div>
-                                <div class="flex-shrink-0">
-                                    <a href="#" class="text-sm font-medium text-cyan-600 hover:bg-gray-100 rounded-lg p-2">View all</a>
+                                    <h3 class="text-xl font-bold text-gray-900 mb-2">Latest Catgeories</h3>
+                                    <span class="text-base font-normal text-gray-500">This is a list of latest categories</span>
                                 </div>
                             </div>
                             <div class="flex flex-col mt-8">
@@ -39,96 +36,34 @@
                                                 <thead class="bg-gray-50">
                                                 <tr>
                                                     <th scope="col" class="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                        Transaction
-                                                    </th>
-                                                    <th scope="col" class="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                        Date & Time
-                                                    </th>
-                                                    <th scope="col" class="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                        Amount
+                                                        Wiki Categories
                                                     </th>
                                                 </tr>
                                                 </thead>
                                                 <tbody class="bg-white">
-                                                <tr>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                                                        Payment from <span class="font-semibold">Bonnie Green</span>
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                                        Apr 23 ,2021
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                                                        $2300
-                                                    </td>
-                                                </tr>
-                                                <tr class="bg-gray-50">
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
-                                                        Payment refund to <span class="font-semibold">#00910</span>
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                                        Apr 23 ,2021
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                                                        -$670
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                                                        Payment failed from <span class="font-semibold">#087651</span>
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                                        Apr 18 ,2021
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                                                        $234
-                                                    </td>
-                                                </tr>
-                                                <tr class="bg-gray-50">
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
-                                                        Payment from <span class="font-semibold">Lana Byrd</span>
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                                        Apr 15 ,2021
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                                                        $5000
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                                                        Payment from <span class="font-semibold">Jese Leos</span>
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                                        Apr 15 ,2021
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                                                        $2300
-                                                    </td>
-                                                </tr>
-                                                <tr class="bg-gray-50">
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
-                                                        Payment from <span class="font-semibold">THEMESBERG LLC</span>
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                                        Apr 11 ,2021
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                                                        $560
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                                                        Payment from <span class="font-semibold">Lana Lysle</span>
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                                        Apr 6 ,2021
-                                                    </td>
-                                                    <td class="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                                                        $1437
-                                                    </td>
-                                                </tr>
+                                                <?php
+                                                $categorie = new Categorie();
+                                                $latestCategories = $categorie->getLatestCategories();
+
+                                                foreach ($latestCategories as $category):
+                                                    ?>
+                                                    <tr>
+                                                        <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                                            <span class="font-semibold flex justify-between">
+                                                                <?php echo $category['categorie']; ?>
+                                                                <span class="text-gray-500 ml-auto">
+                                                                    <?php
+                                                                    $wiki = new Wiki;
+                                                                    echo $wiki->totalWikiCategory($category['categorie_id']);
+                                                                    ?> Articles
+                                                                </span>
+                                                            </span>
+                                                        </td>
+                                                    </tr>
+                                                <?php endforeach; ?>
                                                 </tbody>
                                             </table>
+
                                         </div>
                                     </div>
                                 </div>
