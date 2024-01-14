86b0a633f7c4ff7789af874d1eb28e66c10950fc
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-14 22:41:08 +0100

    update register

--- views/signup_view.php
+++ views/signup_view.php
@@ -1,18 +1,18 @@
 <!-- Container -->
 <div class="container mx-auto">
-    <div class="w-full flex justify-center px-6 my-12 p-12">
+    <div class="w-full flex justify-center bg-black px-6 p-8">
         <!-- Row -->
-        <div class="w-full xl:w-3/4 lg:w-11/12 flex">
+        <div class="w-full xl:w-3/4 lg:w-11/12 p-16 flex">
             <!-- Col -->
             <div
 
-                    class="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
-                    style="background-image: url('assets/img/bg-2.jpg')"
+                    class="w-full h-auto hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+                    style="background-image: url('assets/img/Wiki-black.png')"
             ></div>
             <!-- Col -->
-            <div class="signup w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none shadow-2xl">
+            <div class="signup w-full lg:w-7/12 bg-white backdrop-opacity-50 p-5 rounded-2xl lg:rounded-l-none shadow-3xl" style="opacity: 75%">
                 <h3 class="pt-4 text-2xl text-center">Create an Account!</h3>
-                <form method="post" action="index.php?page=signup" class="form px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                <form method="post" action="index.php?page=signup" class="form px-8 pt-6 pb-8 mb-4 rounded">
                     <div class="mb-4 md:flex md:justify-between">
                         <div class="mb-4 md:mr-2 md:mb-0">
                             <label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
@@ -51,8 +51,7 @@
                                 name="email"
                         required/>
                     </div>
-                    <div class="mb-4 md:flex md:justify-between">
-                        <div class="mb-4 md:mr-2 md:mb-0">
+                    <div class="mb-4">
                             <label class="block mb-2 text-sm font-bold text-gray-700" for="password">
                                 Password
                             </label>
@@ -63,20 +62,6 @@
                                     placeholder="******************"
                                     name="password"
                             required/>
-<!--                            <p class="text-xs italic text-red-500">Please choose a password.</p>-->
-                        </div>
-<!--                        <div class="md:ml-2">-->
-<!--                            <label class="block mb-2 text-sm font-bold text-gray-700" for="c_password">-->
-<!--                                Confirm Password-->
-<!--                            </label>-->
-<!--                            <input-->
-<!--                                    class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"-->
-<!--                                    id="c_password"-->
-<!--                                    type="password"-->
-<!--                                    placeholder="******************"-->
-<!--                                    name="c_password"-->
-<!--                            required/>-->
-<!--                        </div>-->
                     </div>
                     <div class="mb-6 text-center">
                         <button
